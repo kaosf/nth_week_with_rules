afaa8bc923eb476fc858d5388271773072b70332
ka <ka.kaosf@gmail.com>
2023-06-23 16:24:32 +0900

    Fix by Rubocop

--- test/test_nth_week_with_rules.rb
+++ test/test_nth_week_with_rules.rb
@@ -1,6 +1,8 @@
-require 'test_helper'
-require 'nth_week_with_rules'
+require "test_helper"
+require "nth_week_with_rules"
 
+# rubocop:disable Layout/ExtraSpacing
+# rubocop:disable Layout/HashAlignment
 class TestNthWeekWithRules < Test::Unit::TestCase
   data(
     "2016-04-01" => [Date.new(2016,  4,  1), 1],
@@ -11,11 +13,11 @@ class TestNthWeekWithRules < Test::Unit::TestCase
     "2016-04-25" => [Date.new(2016,  4, 25), 5],
     "2016-04-30" => [Date.new(2016,  4, 30), 5],
     "2016-05-01" => [Date.new(2016,  5,  1), 1],
-    "2016-05-31" => [Date.new(2016,  5, 31), 5])
+    "2016-05-31" => [Date.new(2016,  5, 31), 5]
+  )
   test "#nth_week" do |(date, nth)|
     assert { date.nth_week == nth }
   end
-
 
   data(
     "2016-04-01 1" => 1,
@@ -23,7 +25,8 @@ class TestNthWeekWithRules < Test::Unit::TestCase
     "2016-04-01 3" => 3,
     "2016-04-01 4" => 4,
     "2016-04-01 5" => 5,
-    "2016-04-01 6" => 6)
+    "2016-04-01 6" => 6
+  )
   test "#nth_week with first_wday" do |first_wday|
     assert { Date.new(2016, 4, 1).nth_week(first_wday: first_wday) == 1 }
   end
@@ -49,7 +52,8 @@ class TestNthWeekWithRules < Test::Unit::TestCase
     "2016-04-01 5 :many"   => [5, :many,   1],
     "2016-04-01 6 :before" => [6, :before, 5],
     "2016-04-01 6 :after"  => [6, :after,  1],
-    "2016-04-01 6 :many"   => [6, :many,   5])
+    "2016-04-01 6 :many"   => [6, :many,   5]
+  )
   test "#nth_week with first_wday and select_month" do |(first_wday, month_select, nth)|
     assert { Date.new(2016, 4, 1).nth_week(first_wday: first_wday, month_select: month_select) == nth }
   end
@@ -59,7 +63,8 @@ class TestNthWeekWithRules < Test::Unit::TestCase
     "2016-04-03" => [Date.new(2016,  4,  3), 1],
     "2016-04-11" => [Date.new(2016,  4, 11), 2],
     "2016-04-18" => [Date.new(2016,  4, 18), 3],
-    "2016-04-25" => [Date.new(2016,  4, 25), 4])
+    "2016-04-25" => [Date.new(2016,  4, 25), 4]
+  )
   test "#nth_week with base 0" do |(date, nth)|
     assert { date.nth_week(base: 0) == nth }
   end
@@ -83,7 +88,8 @@ class TestNthWeekWithRules < Test::Unit::TestCase
     "2016-04-01 3" => [Date.new(2016,  4,  1), 3, Date.new(2016,  3, 30)],
     "2016-04-01 4" => [Date.new(2016,  4,  1), 4, Date.new(2016,  3, 31)],
     "2016-04-01 5" => [Date.new(2016,  4,  1), 5, Date.new(2016,  4,  1)],
-    "2016-04-01 6" => [Date.new(2016,  4,  1), 6, Date.new(2016,  3, 26)])
+    "2016-04-01 6" => [Date.new(2016,  4,  1), 6, Date.new(2016,  3, 26)]
+  )
   test "#first_date_of_week" do |(date, first_wday, first_date)|
     assert { date.first_date_of_week(first_wday: first_wday) == first_date }
   end
